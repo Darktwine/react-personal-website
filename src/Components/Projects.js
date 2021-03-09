55dd89eed9555768d7fcdbf3a12ec79e88b1f6bf
Kevin <Kevin.FNCW@gmail.com>
2021-03-08 22:59:49 -0500

    fix spacing

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,7 @@ import ToDoList from '../images/todolist.jpg';
 
 const Projects = () => {
   return (
-    <div id="projects" style={{ backgroundColor: '#001a33', color: 'white', paddingBottom: '70px' }}>
+    <div id="projects" style={{ backgroundColor: '#001a33', color: 'white', paddingBottom: '70px', marginTop: '-50px' }}>
       <h1 style={{ textAlign: 'center', paddingTop: '40px', marginBottom: '40px' }}>Projects</h1>
       <CardDeck>
         <Card bg='dark'>
